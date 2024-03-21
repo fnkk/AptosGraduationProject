5bcfc07a77432297712f8a9c0ce4bf6875105e8a
fnkk <1748501327@qq.com>
2024-03-21 15:30:03 +0800

    feat: complete collections

--- client/app/ui/components/navbar.tsx
+++ client/app/ui/components/navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { Layout, Row, Col, Button,} from "antd";
+import { Layout, Row, Col} from "antd";
+import Link from "next/link";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 function Navbar() {
@@ -8,15 +9,15 @@ function Navbar() {
             <Layout>
                 <Row align="middle">
                     <Col span={2} offset={1}>
-                        <Button type="text" className="text-[18px] font-bold">
+                        <Link href="/" className="text-[18px] font-bold">
                             My todolist
-                        </Button>
+                        </Link>
 
                     </Col>
                     <Col span={8} offset={1}>
-                        <Button type="text" className="text-[18px] font-bold">
+                        <Link href="/collections" className="text-[18px] font-bold">
                             My collections
-                        </Button>
+                        </Link>
                     </Col>
                     <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
                         <WalletSelector />
